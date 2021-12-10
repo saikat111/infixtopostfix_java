a9d4415d9ab80e767f441a70ded1ab91ed6b3029
saikat111 <s8856010@gmail.com>
2021-12-10 21:50:13 +0600

    InfixToPostfix

---
+++ src/StackD.java
@@ -0,0 +1,29 @@
+class StackD
+{
+    char a[]=new char[100];
+    int top=-1;
+    void push(char c)
+    {
+        try
+        {
+            a[++top]= c;
+        }
+        catch(StringIndexOutOfBoundsException e)
+        {
+            System.out.println("Stack full, no room to push, size=100");
+            System.exit(0);
+        }
+    }
+    char pop()
+    {
+        return a[top--];
+    }
+    boolean isEmpty()
+    {
+        return (top==-1)?true:false;
+    }
+    char peek()
+    {
+        return a[top];
+    }
+}
